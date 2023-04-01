2f5b640f6b13bc194a08f831d0092d6b8fd837f2
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2023-04-01 14:31:19 -0400

    Testing bugfix

--- app.py
+++ app.py
@@ -279,9 +279,12 @@ def create_app(test_config=None):
     return app
 
 
+app = create_app()
 
+"""
 if __name__ == '__main__':
     app = create_app()
 
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+"""
