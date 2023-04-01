004a62c7a1f98a2e4ccb642c43b000b50c3fcd26
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2023-04-01 16:49:25 -0400

    Update models.py

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ database_name = os.getenv('DATABASE_NAME')
 database_user = os.getenv('DATABASE_USER')
 database_password = os.getenv('DATABASE_PASSWORD')
 database_host = os.getenv('DATABASE_HOST')
-database_path = f"postgresql://{database_user}:{database_password}@{database_host}/{database_name}"
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
