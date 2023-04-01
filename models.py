21b87b10e98057f274d65b3d6e217fc835b6a713
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2023-04-01 14:35:24 -0400

    Update for testing bugfix

--- models.py
+++ models.py
@@ -9,7 +9,8 @@ database_name = os.getenv('DATABASE_NAME')
 database_user = os.getenv('DATABASE_USER')
 database_password = os.getenv('DATABASE_PASSWORD')
 database_host = os.getenv('DATABASE_HOST')
-database_path = f"postgresql://{database_user}:{database_password}@{database_host}/{database_name}"
+database_path = f"postgresql://postgres:password123@localhost:5432/final"
+# database_path = f"postgresql://{database_user}:{database_password}@{database_host}/{database_name}"
 
 db = SQLAlchemy()
 
