a72ed0023e30dff455817fdc5bcc8906053c60ce
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2023-04-02 09:37:42 -0400

    Updates to models.py

--- models.py
+++ models.py
@@ -2,13 +2,7 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
-from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='dbinfo.env')
-database_name = os.getenv('DATABASE_NAME')
-database_user = os.getenv('DATABASE_USER')
-database_password = os.getenv('DATABASE_PASSWORD')
-database_host = os.getenv('DATABASE_HOST')
 database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:password123@localhost:5432/final"
+export DATABASE_URL="postgres://mwahhaqtdmkzca:7cdf396738cc245804a8e8a75b601462e08d02a001fa9f309736d14f05744969@ec2-3-210-173-88.compute-1.amazonaws.com:5432/dqb60fkinl99i"
 export EXCITED="true"
 
 echo $DATABASE_URL
