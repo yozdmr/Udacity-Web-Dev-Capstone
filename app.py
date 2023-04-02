cbd4adef0a2e82a6ece12082f3b35ecb6b3f05fe
Yusuf Ozdemir <yusuf.ozdemir.0713@gmail.com>
2023-04-02 13:24:31 -0400

    Final cleanup

--- app.py
+++ app.py
@@ -281,10 +281,6 @@ def create_app(test_config=None):
 
 app = create_app()
 
-"""
-if __name__ == '__main__':
-    app = create_app()
 
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-"""
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6969, debug=True)
